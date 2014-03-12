3a7f135a832c026c95f93dfb7c2bb817efbfad7d
vkuzma <kuzma@allink.ch>
2014-03-12 18:16:50 +0100

    working on frontend

---
+++ allink_frontend_editor/static/allink_frontend_editor/javascript/allink_frontend_editor.js
@@ -0,0 +1,206 @@
+(function($) {
+    $(function() {
+        new FrontendEditor();
+    });
+
+    var FrontendEditor = function() {
+        this.initDom();
+        this.initActions();
+    };
+
+    FrontendEditor.prototype = {
+        initDom: function() {
+            this.$panel = $('#frontend-editor-panel');
+            
+            this.inline_edit = $('.frontend-editor-edit.inline');
+            this.richtext_edit = $('.frontend-editor-edit.lightbox');
+
+            this.active_anchor = null;
+
+            this.save_btn = $('#live-edit-save');
+
+            this.lightbox_container = $('#frontend-editor-lightbox');
+
+            this.edit_start_btn = $('#live-edit-start');
+            this.edit_stop_btn = $('#live-edit-stop');
+
+            this.updated_warning = $('#updated-warning');
+        },
+        // init user actions
+        initActions: function() {
+            this.edit_start_btn.click($.proxy(this.startEditing, this));
+            this.edit_stop_btn.click($.proxy(this.stopEditing, this));
+
+            this.selectedContent = null;
+            var self = this;
+
+            // focusin inline edit
+            this.inline_edit.focusin(function() {
+                self.selectedContent = $(this).html();
+            }).focusout(function() {
+                var $this = $(this);
+                // selected content stayed the same, do not save
+                if(self.selectedContent == $this.html())
+                    return;
+
+                // if its not richtext, then call striptags
+                if(!$this.hasClass('tinymce'))
+                    $this.html(self.stripTag($this.html()));
+
+                // save data
+                self.saveData();
+
+                self.selectedContent = null;
+            });
+        },
+        startEditing: function() {
+            this.inline_edit.attr('contenteditable', 'true').addClass('marked-editable');
+            this.richtext_edit.addClass('marked-editable');
+
+            this.edit_stop_btn.show();
+            this.edit_start_btn.hide();
+
+            tinymce.init(window.tinymce_inline_config);
+
+            var self = this;
+
+            // open richtext editor
+            this.richtext_edit.click(function() {
+                event.preventDefault();
+                self.current_richtext = $(this);
+                self.active_identifier = $(this).attr('data-identifier');
+                self.openRichtextInLightbox(self.active_identifier);
+            });
+        },
+        stopEditing: function() {
+            this.inline_edit.attr('contenteditable', 'false').removeClass('marked-editable');
+            this.richtext_edit.removeClass('marked-editable');
+
+            this.edit_stop_btn.hide();
+            this.edit_start_btn.show();
+
+            tinymce.remove('span.tinymce.inline');
+
+            this.richtext_edit.unbind();
+        },
+        // save inline data
+        saveData: function() {
+            var inline_text_list = '';
+            this.inline_edit.each($.proxy(function(key, value) {
+                var $value = $(value);
+                var stripped_tring = $value.html();
+                if(!$value.hasClass('tinymce'))
+                    stripped_tring = this.stripTag($value.html());
+                inline_text_list += $value.attr('data-identifier') + "=" + escape(stripped_tring) + '&';
+            }, this));
+
+            $.ajax({
+                url: '/api/save/',
+                method: 'POST',
+                data: inline_text_list
+            });
+            this.showQuickInfo();
+        },
+        // display small window to show that data is saving
+        showQuickInfo: function(text) {
+            if(!this.save_progress_view)
+                this.save_progress_view = this.$panel.find('.save-progress');
+
+            if(!this.quick_info_content)
+                this.quick_info_content = this.$panel.find('.quick-info-content');
+            
+            // this.quick_info_content.html(text);
+                
+            this.save_progress_view.fadeIn();
+            setTimeout($.proxy(function() {
+                this.save_progress_view.fadeOut();
+            }, this), 1500);
+        },
+        showUpdatedWarning: function() {
+            this.updated_warning.show();
+        },
+        stripTag: function(text) {
+            return text.replace(/(?!<br>)(<([^>]+)>)/ig,"");
+        },
+        openRichtextInLightbox: function(identifier) {
+            var self = this;
+            $.ajax({
+                url: '/admin/page/page/1/snippets/' + identifier + '/',
+                success: function(result) {
+                    self.lightbox_container.find('.content').html(result);
+                    self.tinymce = tinymce.init(window.tinymce_config);
+                    self.lightbox_container.show();
+                    // lightbox save
+                    self.lightbox_container.find('form').submit(function(event) {
+                        event.preventDefault();
+                        var data = $(this).serialize();
+
+                        self.current_richtext.html(tinymce.activeEditor.getContent());
+
+                        data += '&data=' + tinymce.activeEditor.getContent();
+                        $.post('/admin/page/page/1/snippets/' + identifier + '/', data,
+                            function(result) {
+                                self.showQuickInfo(result);
+                            });
+                        self.cancelLightBox();
+                    });
+                    // lightbox close
+                    self.lightbox_container.find('.cancel').click(function(event) {
+                        event.preventDefault();
+                        self.cancelLightBox();
+                    });
+                }
+            });
+
+        },
+        cancelLightBox: function() {
+            this.lightbox_container.hide();
+        },
+    };
+
+    function getCookie(name) {
+        var cookieValue = null;
+        if(document.cookie && document.cookie) {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = jQuery.trim(cookies[i]);
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+
+    var csrftoken = getCookie('csrftoken');
+
+    function csrfSafeMethod(method) {
+        // these HTTP methods do not require CSRF protection
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    }
+    function sameOrigin(url) {
+        // test that a given url is a same-origin URL
+        // url could be relative or scheme relative or absolute
+        var host = document.location.host; // host + port
+        var protocol = document.location.protocol;
+        var sr_origin = '//' + host;
+        var origin = protocol + sr_origin;
+        // Allow absolute or scheme relative URLs to same origin
+        return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
+            (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
+            // or any other URL that isn't scheme relative or absolute i.e relative.
+            !(/^(\/\/|http:|https:).*/.test(url));
+    }
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!csrfSafeMethod(settings.type) && sameOrigin(settings.url)) {
+                // Send the token to same-origin, relative URLs only.
+                // Send the token only if the method warrants CSRF protection
+                // Using the CSRFToken value acquired earlier
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+        }
+    });
+})($);
--- allink_frontend_editor/templatetags/frontend_editor_tags.py
+++ allink_frontend_editor/templatetags/frontend_editor_tags.py
@@ -6,10 +6,10 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def template_snippet(context, identifier, page):
-    content = page._snippets(identifier, '')
-    if not context['request'].user.is_staff():
+    content = page._snippets.get(identifier, '')
+    if not context['request'].user.is_staff:
         return content
-    return '<span live-editable-richtext data-identifier=%(identifier)s>%(content)s</span><button href="" btn-data-identifier=%(identifier)s class="live-edit-richtext-btn">edit richtext</button>' % {
+    return '<span class="frontend-editor-edit lightbox" live-editable-richtext data-identifier=%(identifier)s>%(content)s</span><button href="" btn-data-identifier=%(identifier)s class="live-edit-richtext-btn">edit richtext</button>' % {
         'identifier': identifier,
         'content': content,
     }
@@ -17,6 +17,6 @@ def template_snippet(context, identifier, page):
 
 @register.simple_tag(takes_context=True)
 def frontend_editor_panel(context):
-    if context['request'].user.is_staff():
+    if context['request'].user.is_staff:
         return render_to_string('allink_frontend_editor/panel.html', context)
     return ''
