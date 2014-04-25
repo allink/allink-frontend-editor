84855334bf0a07d8b0d9e9023c980c88a52e2e65
Marc Egli <frog32@me.com>
2014-04-25 11:05:09 +0200

    fix for django<1.6

--- allink_frontend_editor/page_extensions/snippets.py
+++ allink_frontend_editor/page_extensions/snippets.py
@@ -43,8 +43,7 @@ class Extension(extensions.Extension):
                 def wrapper(*args, **kwargs):
                     return inner_self.admin_site.admin_view(view)(*args, **kwargs)
                 return update_wrapper(wrapper, view)
-
-            info = inner_self.model._meta.app_label, inner_self.model._meta.model_name
+            info = inner_self.model._meta.app_label, getattr(inner_self.model._meta, 'model_name', inner_self.model._meta.module_name)  # django <1.6 had this as module_name
 
             urlpatterns = patterns('',
                                    url(r'^(.+)/snippets/([\w_]+)/$', wrap(inner_self.snippets_view), name='%s_%s_snippets' % info),
