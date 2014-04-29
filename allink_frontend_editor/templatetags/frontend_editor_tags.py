ef0750f2e891b674e29b43d005c51deaf012b052
vkuzma <kuzma@allink.ch>
2014-04-29 09:46:43 +0200

    added feature to handle raw html data

--- allink_frontend_editor/templatetags/frontend_editor_tags.py
+++ allink_frontend_editor/templatetags/frontend_editor_tags.py
@@ -1,14 +1,17 @@
 from django import template
 from django.template.loader import render_to_string
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
 
 @register.simple_tag(takes_context=True)
-def template_snippet(context, identifier, page, editing_type='inline'):
+def template_snippet(context, identifier, page, editing_type='inline', raw=False):
     content = page._snippets.get(identifier, '')
     print content
     if not context['request'].user.is_staff:
+        if raw:
+            return content.replace('&amp;', '&').replace('&lt;', '<').replace('&gt;', '>')
         return content
     return '<span class="frontend-editor-edit %(editing_type)s" data-identifier=%(identifier)s><a class="frontend-editor-btn" href="#"><span class="frontend-editor-edit-icon"></span></a><span class="frontend-editor-content">%(content)s</span></span>' % {
         'identifier': identifier,
